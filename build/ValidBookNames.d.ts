c17b3f1046b627d737fe4eeca4511dd27ba3058e
Phillip Burch <phillip.burch@live.com>
2019-10-25 23:13:48 -0500

    Remove build dir

--- build/ValidBookNames.d.ts
+++
@@ -1,5 +0,0 @@
-declare type BookMap = {
-    [p: string]: string;
-};
-declare const _default: BookMap;
-export default _default;
--- build/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Hit {
-    startIdx: number;
-    endIdx: number;
-    osis: string;
-    text: string;
-}
-export declare const parseText: (text: string) => Hit[];
